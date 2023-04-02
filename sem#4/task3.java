286be6f3f5199573c135aff1098bb965c1149226
NIKITA LITVINENKO <litvinovnick24@gmail.com>
2023-04-02 21:16:44 +0300

    task3 completed

--- sem#4/task3.java
+++ sem#4/task3.java
@@ -1,22 +1,74 @@
+import java.util.LinkedList;
 import java.util.Scanner;
+
+/*
+Задача 3. В калькулятор добавьте возможность отменить последнюю операцию.
+ * Ввод: 1
+Ввод: +
+Ввод: 2
+Вывод: 3
+Ввод:+
+Ввод:4
+Вывод 7
+Ввод:*
+Ввод:3
+Вывод 21
+Отмена
+Вывод 7
+Отмена
+Вывод 3
+Ввод:-
+Ввод:1
+Вывод 2
+ */
 
 public class task3 {
     public static void main(String[] args) {
-        Scanner term = new Scanner(System.in);
-        System.out.print("Ввод первого числа:");
-        int num1 = term.nextInt();
-        System.out.print("Ввод операции:");
-        char oper = term.next().charAt(0);
-        System.out.print("Ввод второго числа:");
-        int num2 = term.nextInt();
-        int res = 0;
-
-        switch(oper) {
-            case '+' : res = num1 + num2;break;
-            case '-' : res = num1 - num2;break;
-            case '/' : res = num1 / num2;break;
-            case '*' : res = num1 * num2;break;
+        Scanner sc = new Scanner(System.in, "Cp866");
+        LinkedList<Integer> results = new LinkedList<>();
+        int first;
+        int second;
+        String zn;
+        boolean firts_or_not = true;
+        while(true){
+            if (firts_or_not==true){
+                System.out.print("Ввод числа ->");
+                first = sc.nextInt();
+                sc.nextLine();
+                firts_or_not = false;
+            }
+            else {
+                first = results.getLast();
+            }
+            System.out.print("Ввод знака ->");
+            zn = sc.nextLine();
+            if(zn.equals("Отмена")){
+                results.removeLast();
+                System.out.println(results.getLast());
+                if(results.size()==0) firts_or_not=true;
+                continue;
+            }
+            if(zn.equals("Стоп")) break;
+            System.out.print("Ввод числа ->");
+            second = sc.nextInt();
+            sc.nextLine();
+            if (zn.equals("+")) {
+                System.out.println((first+second));
+                results.add(first+second);
+            }
+            else if (zn.equals("-")){
+                System.out.println(first-second);
+                results.add(first-second);
+            }
+            else if (zn.equals("*")) {
+                System.out.println(first*second);
+                results.add(first*second);
+            }
+            else if (zn.equals("/")) {
+                System.out.println(first/second);
+                results.add(first-second);
+            }
         }
-        System.out.printf("Вывод результата: %d", res);
+        sc.close();
     }
 }
